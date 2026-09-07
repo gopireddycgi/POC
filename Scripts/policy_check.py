fd75bd109e24d02030c3c8c762ba7ff5c72407b9
gopireddycgi <corp.ygr@gmail.com>
2026-09-08 00:17:11 +0530

    first commit

---
+++ Scripts/policy_check.py
@@ -0,0 +1,24 @@
+import yaml
+import json
+import sys
+ 
+with open("policies/resilience_rules.yaml") as f:
+    policy = yaml.safe_load(f)
+ 
+with open("reports/result.json") as f:
+    result = json.load(f)
+ 
+required_score = policy["scoring"]["passing_score"]
+ 
+actual_score = result["score"]
+ 
+print(f"Required Score : {required_score}")
+print(f"Actual Score   : {actual_score}")
+ 
+if actual_score < required_score:
+    print("FAILED: Deployment Blocked")
+    sys.exit(1)
+ 
+print("PASSED: Deployment Approved")
+ 
+ 
